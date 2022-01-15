787336f62b9f71a46026df8de7a8e0c7f9067400
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-15 14:06:30 -0500

    commit tree eb21f72d87a6a2b32abca46347af0be9fe5a3ef4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+"strict"
+document.addEventListener("DOMContentLoaded", () => {
+  const changeParagraph = document.querySelector("#text")
+  changeParagraph.textContent = "This is really cool!"
+})
+
+console.log("this loaded first")
